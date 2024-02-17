32e9dd5e35c91966769527558d2c1c250d7f935e
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2024-02-17 19:28:41 -0300

    Initial commit

---
+++ src/main/java/com/arturrdc/issuescoutbackend/IssueScoutBackendApplication.java
@@ -0,0 +1,13 @@
+package com.arturrdc.issuescoutbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueScoutBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IssueScoutBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arturrdc/issuescoutbackend/IssueScoutBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arturrdc.issuescoutbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueScoutBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
