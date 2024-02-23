355e002bf1c070a2f9b99fbf2c5fb5d04f7543cf
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2024-02-23 01:06:17 -0300

    Receive requests from new comments

---
+++ src/main/java/com/arturrdc/issuescoutbackend/ticket/CommentDTO.java
@@ -0,0 +1,40 @@
+package com.arturrdc.issuescoutbackend.ticket;
+
+public class CommentDTO {
+    private String content;
+    private String author;
+    private String createdAt;
+
+    public CommentDTO(String content, String author, String createdAt) {
+        this.content = content;
+        this.author = author;
+        this.createdAt = createdAt;
+    }
+
+    public CommentDTO() {
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+}
--- src/main/java/com/arturrdc/issuescoutbackend/ticket/TicketController.java
+++ src/main/java/com/arturrdc/issuescoutbackend/ticket/TicketController.java
@@ -4,6 +4,7 @@ import com.arturrdc.issuescoutbackend.project.Project;
 import com.arturrdc.issuescoutbackend.project.ProjectService;
 import com.arturrdc.issuescoutbackend.ticket.TicketDTO;
 import com.arturrdc.issuescoutbackend.user.UserDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,11 +24,13 @@ public class TicketController {
 
     @CrossOrigin(origins = "*")
     @GetMapping("/{id}")
-    public TicketDTO getTicket(@PathVariable Long id) {
+    public Map<String, Object> getTicket(@PathVariable Long id) {
 
         TicketDTO ticket = new TicketDTO(1L, "Fix login bug", "Username escape characters", new UserDTO(1L, "John Doe"), "High", "Medium", "example.png", "Bug", "Jane Smith", "Open", "21 May 24","21 May 24 14:59");
-
-        return ticket;
+        Map<String, Object> response = new HashMap<>();
+        response.put("details", ticket);
+        response.put("comments", new CommentDTO[]{new CommentDTO("comment1", "John Doe", "21 May 24 16:59"), new CommentDTO("comment2", "Jane Smith", "21 May 24 17:10") });
+        return response;
     }
     @CrossOrigin(origins = "*")
     @PostMapping("/{id}")
@@ -46,6 +49,14 @@ public class TicketController {
         System.out.println("deleted ticket with id " + id);
     }
 
+    @CrossOrigin(origins = "*")
+    @PostMapping("/{id}/comments")
+    public ResponseEntity<CommentDTO> addComment(@PathVariable Long id, @RequestBody CommentDTO commentDTO) {
+
+//        TicketDTO ticket = new TicketDTO(1L, "Fix login bug", "Username escape characters", new UserDTO(1L, "John Doe"), "High", "Medium", "example.png", "Bug", "Jane Smith", "Open","21 May 24","21 May 24 14:59");
+        System.out.println("Added a new comment in project " + id);
+        return ResponseEntity.status(HttpStatus.CREATED).body(commentDTO);
+    }
 
 
 
