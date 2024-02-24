065a84f59ba6097c6aa6d7018c020a4ee2fd4f91
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2024-02-24 11:39:07 -0300

    Add auth payload objects (pojos)

---
+++ src/main/java/com/arturrdc/issuescoutbackend/security/payload/response/MessageResponse.java
@@ -0,0 +1,13 @@
+package com.arturrdc.issuescoutbackend.security.payload.response;
+
+public class MessageResponse {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
