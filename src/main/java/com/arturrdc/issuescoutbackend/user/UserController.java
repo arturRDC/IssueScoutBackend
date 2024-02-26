4b761201cfda0ff2349864b82af9e743f6eb717b
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2024-02-26 20:40:09 -0300

    Changed email info

--- src/main/java/com/arturrdc/issuescoutbackend/user/UserController.java
+++ src/main/java/com/arturrdc/issuescoutbackend/user/UserController.java
@@ -20,14 +20,7 @@ public class UserController {
         this.userService = userService;
         this.mapperService = mapperService;
     }
-//    private static final String[] TEAM_MEMBERS = {
-//        "Alex,https://reqres.in/img/faces/1-image.jpg,alex@dashwind.com,Developer,19 May 2024,5 hr ago",
-//                "Ereena,https://reqres.in/img/faces/2-image.jpg,ereena@dashwind.com,Developer,18 May 2024,15 min ago",
-//                "John,https://reqres.in/img/faces/3-image.jpg,john@dashwind.com,Developer,17 May 2024,20 hr ago",
-//                "Matrix,https://reqres.in/img/faces/4-image.jpg,matrix@dashwind.com,Manager,16 May 2024,1 hr ago",
-//                "Virat,https://reqres.in/img/faces/5-image.jpg,virat@dashwind.com,Submitter,15 May 2024,40 min ago",
-//                "Miya,https://reqres.in/img/faces/6-image.jpg,miya@dashwind.com,Submitter,13 May 2024,5 hr ago"
-//    };
+
 
 
     @GetMapping("names")
